714060a77d8cf2d9c70f30a30e1829c1859e8a08
khzband <khzband@gmail.com>
2026-04-13 19:44:46 +0300

    incapsulation upgrade

--- Assets/Scripts/Model/UIModel.cs
+++ Assets/Scripts/Model/UIModel.cs
@@ -3,85 +3,117 @@ using UnityEngine;
 public class UIModel : IService
 {
     EventBus eventBus;
-    
-    public int page {  get; private set; }
-    public int mode { get; private set; }
-    public int color { get; private set; }
-    public int instrument { get; private set; } // Номер интсрумента в соостветсвующем списке InstrumentController
-    public int stage { get; private set; } // 0 - начальный режим, 1 - фаза GetReady
 
-    public bool inputBlocked { get; private set; }
-    public bool instrumentBlocked {  get; private set; }
-    public bool quickColorReset { get; private set; }
+    private int _page;
+    private int _mode;
+    private int _color;
+    private int _instrument; // Номер интсрумента в соостветсвующем списке InstrumentController
+    private int _stage; // 0 - начальный режим, 1 - фаза GetReady
+
+    private bool _inputBlocked;
+    private bool _instrumentBlocked;
+    private bool _quickColorReset;
+
+
+    public int page => _page;
+    public int mode => _mode;
+    public int color => _color;
+    public int instrument => _instrument; // Номер инструмента в соостветсвующем списке InstrumentController
+    public int stage => _stage; // 0 - начальный режим, 1 - фаза GetReady
+
+    public bool inputBlocked => _inputBlocked;
+    public bool instrumentBlocked => _instrumentBlocked;
+    public bool quickColorReset => _quickColorReset;
 
     public void Init()
     {
         eventBus = ServiceLocator.Instance.Get<EventBus>();
 
         
-        page = 1; // 0 - powder, 1 - blush, 2 - lipstick, 3 - eyeshadows
-        mode = 1; // 0 - powder, 1 - blush, 2 - lipstick, 3 - eyeshadows, 4 - cream
-        color = 0;
-        stage = 0;
+        _page = 1; // 0 - powder, 1 - blush, 2 - lipstick, 3 - eyeshadows
+        _mode = 1; // 0 - powder, 1 - blush, 2 - lipstick, 3 - eyeshadows, 4 - cream
+        _color = 0;
+        _stage = 0;
 
-        inputBlocked = false;
-        instrumentBlocked = true;
-        quickColorReset = false;
+        _inputBlocked = false;
+        _instrumentBlocked = true;
+        _quickColorReset = false;
     }
 
     public void SetPage(int pageValue)
     {
-        page = pageValue;
+        if (pageValue >= 0 && pageValue < 4) 
+        {
+            _page = pageValue;
+        }
+
+        
     }
 
     public void SetMode(int modeValue)
     {
-        mode = modeValue;
+        if (modeValue >= 0 && modeValue < 5)
+        {
+            _mode = modeValue;
+        }
+        
     }
 
     public void SetColor(int newColor)
     {
-        color = newColor;
+        if (newColor >= 0 && newColor < 10)
+        {
+            _color = newColor;
+        }
+        
     }
 
     public void SetQuickColorReset(bool value)
     {
-        quickColorReset = value;
+        _quickColorReset = value;
     }
 
     public void SetStage(int newStage)
     {
-        stage = newStage;
+        if (newStage >= 0 && newStage < 2)
+        {
+            _stage = newStage;
+        }
+        
     }
 
     public void SetInstrument(int newInstrument) 
-    { 
-        instrument = newInstrument;
-        inputBlocked = true;
-        eventBus.instrumentSelected?.Invoke();
+    {
+        if (newInstrument >= 0 && newInstrument < 6)
+        {
+            _instrument = newInstrument;
+            _inputBlocked = true;
+            eventBus.instrumentSelected?.Invoke();
+        }
+        
     }
 
     public void BlockInstrumentInput()
     {
-        instrumentBlocked = true;
+        _instrumentBlocked = true;
         Debug.Log("Instrument blocked");
     }
 
     public void UnblockInstrumentInput()
     {
-        instrumentBlocked = false;
+        _instrumentBlocked = false;
         Debug.Log("Instrument unblocked");
     }
 
     public void BlockInput()
     {
-        inputBlocked = true;
+        _inputBlocked = true;
         Debug.Log("Input blocked");
     }
 
     public void UnblockInput() 
     {
-        inputBlocked = false;
+        _inputBlocked = false;
         Debug.Log("Input unblocked");
     }
 
